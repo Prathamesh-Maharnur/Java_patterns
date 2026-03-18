e509319f014af84327d593c58b9ca584190a6616
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-03-18 22:00:39 +0530

    modified Patternq

--- code/Pattern1.java
+++ code/Pattern1.java
@@ -4,7 +4,7 @@ public class Pattern1{
 		int n=5;
 		
 		for(int i=1; i<=n; i++){
-			for(int j=1; j<5; j++){
+			for(int j=1; j<n; j++){
 				System.out.print("* ");
 			}
 			System.out.println();
