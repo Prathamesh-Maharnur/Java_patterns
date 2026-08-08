38dc0d968f15906b3cdcdc19bfeb9bb07a7942a5
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-08-08 17:14:58 +0530

    upadted readme and renamed pattern 1, 2, 4, 5 -> 01, 02, 04, 05

--- code/Pattern04.java
+++ code/Pattern04.java
@@ -1,10 +1,10 @@
-public class Pattern2{
+public class Pattern04{
 	public static void main(String[] args){
-		int n=5;
+		int n =5;
 		
 		for(int i=1; i<=n; i++){
 			for(int j=1; j<=i; j++){
-				System.out.print("* ");
+				System.out.print(j+" ");
 			}
 			System.out.println();
 		}
--- code/Pattern05.java
+++ code/Pattern05.java
@@ -1,4 +1,4 @@
-class Pattern5{
+class Pattern05{
 	public static void main(String[] args){
 		int n = 5;
 		
